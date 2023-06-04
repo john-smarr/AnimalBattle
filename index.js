f7998fdb040549a041759d4ab5967feeda2dae0f
John S <john.smarr97@gmail.com>
2023-06-03 22:19:35 -0400

    beginnings of landing page

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const http = require("http");
-const app = require("./app");
-const server = http.createServer(app);
+const express = require("express")
+const path = require("path")
+
+const app = express()
 
 const {API_PORT} = process.env;
-const port = process.env.port || API_PORT
+const port = process.env.port || API_PORT || 5000
+
+app.use('/public' , express.static('public'));
 
 //server listening
-server.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+app.get('/',(req, res)=> {
+    res.sendFile(path.resolve(__dirname, './pages/homepage.html'))
+})
+
+app.get('/about',(req, res)=>{
+    res.send("Created and maintained by John Smarr. This project was made using Node.js and Express as well as HTML, CSS, and Javascript. ")
+})
+
+app.all('*', (req, res)=> {
+    res.status(404).send('resource not found')
+})
+
+app.listen(5000, ()=>{
+    console.log("listening on 5000....")
+})
